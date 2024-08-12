8ce76f32eaeecb26bbf78fd3f794882794edec58
Oleksii Sapronov <oleksiisapronov@outlook.com>
2024-08-12 07:50:30 -0700

    Add Scroll up function and cookie policy snack bar.

---
+++ resources/js/Components/ScrollUp.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="scrollTopButton" class="fixed w-full flex justify-end bottom-0 pb-16 pr-8 invisible">
+    <button 
+      @click="scrollToTop"
+      class="bg-white hover:bg-indigo-500 focus:outline-none font-medium rounded-full text-sm p-4 text-center dark:bg-blue-600 dark:hover:bg-blue-700 min-h-4 hover:text-white text-indigo-800 border shadow-lg hover:transition-colors">
+      <svg class="w-6 h-6" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+        fill="none" viewBox="0 0 14 8">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+          d="M13 7 7.674 1.3a.91.91 0 0 0-1.348 0L1 7" />
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, onUnmounted, ref } from "vue";
+
+let scrollTopButton = ref(null);
+
+const scrollToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
+
+const handleScroll = () => {
+  if (window.scrollY > 100) {
+    scrollTopButton.value.classList.remove("invisible");
+  } else {
+    scrollTopButton.value.classList.add("invisible");
+  }
+};
+
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+onUnmounted(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+</script>
