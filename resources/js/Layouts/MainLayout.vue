cf7ea64ccb0c573c224012418773c1bdaa2a5ca0
Oleksii Sapronov <oleksiisapronov@outlook.com>
2024-08-12 07:51:05 -0700

    Change nav bar font color.

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 import NavLink from "@/Components/NavLink.vue";
 import ResponsiveNavLink from "@/Components/ResponsiveNavLink.vue";
 import { Link } from "@inertiajs/vue3";
+import ScrollUp from "@/Components/ScrollUp.vue";
 
 const showingNavigationDropdown = ref(false);
 </script>
@@ -10,7 +11,7 @@ const showingNavigationDropdown = ref(false);
 <template>
   <div>
     <div class="min-h-screen bg-gray-100">
-      <nav class="bg-nav border-b border-gray-100">
+      <nav class="bg-nav shadow-lg">
         <!-- Primary Navigation Menu -->
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div class="flex justify-between h-20">
@@ -18,7 +19,7 @@ const showingNavigationDropdown = ref(false);
               <!-- Logo -->
               <div class="shrink-0 flex items-center">
                 <Link :href="route('home')">
-                  <span class="logo-font">AZconvert.com</span>
+                <span class="logo-font">AZconvert.com</span>
                 </Link>
               </div>
             </div>
@@ -30,46 +31,24 @@ const showingNavigationDropdown = ref(false);
               <NavLink :href="route('contact')" :active="route().current('contact')">
                 Contact
               </NavLink>
-              <NavLink
-                :href="route('cookies-policy')"
-                :active="route().current('cookies-policy')"
-              >
+              <NavLink :href="route('cookies-policy')" :active="route().current('cookies-policy')">
                 Cookies policy
               </NavLink>
             </div>
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
-              <button
-                @click="showingNavigationDropdown = !showingNavigationDropdown"
-                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out"
-              >
-                <svg
-                  class="h-6 w-6"
-                  stroke="currentColor"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <path
-                    :class="{
-                      hidden: showingNavigationDropdown,
-                      'inline-flex': !showingNavigationDropdown,
-                    }"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M4 6h16M4 12h16M4 18h16"
-                  />
-                  <path
-                    :class="{
-                      hidden: !showingNavigationDropdown,
-                      'inline-flex': showingNavigationDropdown,
-                    }"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M6 18L18 6M6 6l12 12"
-                  />
+              <button @click="showingNavigationDropdown = !showingNavigationDropdown"
+                class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+                <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
+                  <path :class="{
+                    hidden: showingNavigationDropdown,
+                    'inline-flex': !showingNavigationDropdown,
+                  }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                  <path :class="{
+                    hidden: !showingNavigationDropdown,
+                    'inline-flex': showingNavigationDropdown,
+                  }" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
             </div>
@@ -77,27 +56,18 @@ const showingNavigationDropdown = ref(false);
         </div>
 
         <!-- Responsive Navigation Menu -->
-        <div
-          :class="{
-            block: showingNavigationDropdown,
-            hidden: !showingNavigationDropdown,
-          }"
-          class="sm:hidden"
-        >
+        <div :class="{
+          block: showingNavigationDropdown,
+          hidden: !showingNavigationDropdown,
+        }" class="sm:hidden">
           <div class="pt-2 pb-3 space-y-1">
             <ResponsiveNavLink :href="route('home')" :active="route().current('home')">
               Home
             </ResponsiveNavLink>
-            <ResponsiveNavLink
-              :href="route('contact')"
-              :active="route().current('contact')"
-            >
+            <ResponsiveNavLink :href="route('contact')" :active="route().current('contact')">
               Contact
             </ResponsiveNavLink>
-            <ResponsiveNavLink
-              :href="route('cookies-policy')"
-              :active="route().current('cookies-policy')"
-            >
+            <ResponsiveNavLink :href="route('cookies-policy')" :active="route().current('cookies-policy')">
               Cookies Policy
             </ResponsiveNavLink>
           </div>
