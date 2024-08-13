f1e5762145f267ba5aac8525001276e153027294
Oleksii Sapronov <oleksiisapronov@outlook.com>
2024-08-12 19:33:51 -0700

    Optimize utility functions.

--- resources/js/utils/index.ts
+++ resources/js/utils/index.ts
@@ -8,11 +8,10 @@ export const SERVER_URL = "https://phpstack-1255990-4653753.cloudwaysapps.com";
  * @param src Country
  * @output chinese-yuan-renminbi-cny-page
  */
-export const genCurrenyLink = (src: Country): string => {
+export const genCurrencyLink = (src: Country): string => {
   let out = "";
-  if (src.call != "") out += src.call + "-";
-  out += src.currency_name.replace(' ', '-')
-  out += '-' + src.currency_code + '-page';
+  if (src.call !== "") out += src.call + "-";
+  out += src.currency_name.replace(' ', '-') + '-' + src.currency_code + '-page';
   return "/currencies/pairs/" + out.toLowerCase();
 }
 
@@ -23,14 +22,15 @@ export const genCurrenyLink = (src: Country): string => {
  * @output 1-romanian-leu-ron-to-bangladeshi-taka-bdt
  */
 export const genConvertLink = (src: Country, dest: Country, value: number = 1): string => {
-  let out = `${value}-`
-  if (src.call != "") out += src.call + "-";
-  out += src.currency_name.replace(' ', '-')
-  out += '-' + src.currency_code;
-  out += '-to-';
-  if (dest.call != "") out += dest.call + "-";
-  out += dest.currency_name.replace(' ', '-')
-  out += '-' + dest.currency_code;
+  let out = `${value}-`;
+  if (src.call !== "") {
+    out += src.call + "-";
+  }
+  out += src.currency_name.replace(' ', '-') + '-' + src.currency_code;
+  if (dest.call !== "") {
+    out += '-' + dest.call + "-";
+  }
+  out += dest.currency_name.replace(' ', '-') + '-' + dest.currency_code;
   return "/currencies/pairs/" + out.toLowerCase();
 }
 
@@ -50,3 +50,7 @@ export const genCalculatorLink = (country: Country): string => {
 export const genFlagUrl = (code: string): string => {
   return `https://flagcdn.com/32x24/${code.toLowerCase()}.png`;
 };
+
+export const genCurrencyFullName = (country: Country): string => {
+  return `${country.call} ${country.currency_name} ${country.currency_code}`;
+}
